d0297fe543672d9cfc2d34af295c89ad08afbe94
RevakoOA <revakooa@gmail.com>
2023-08-24 20:26:50 +0300

    Final result of passed codelab. (#1)
    * Add ExoPlayer initialization.
    
     - ExoPlayer is created, bound to view and filled with 1 MediaItem to play the sound.
    
    * Now App can play single track.
    
     - Add state restoring to ExoPlayer. Playback position and play/pause state is restored on reopening the activity.
    
    * Add playlist.
    
     - App now can play both audio and video files and switch between them.
    
    * Add dash video to the playlist.
    
     - DASH video format allows to change video quality on fly.
    
    * Add listeners to be notified when player change state.
    
     - Add state listener for technical purposes.
     - Add analyticsListener to gather data about user experience.

--- exoplayer-codelab-my/src/main/java/com/ostapr/exoplayer_codelab_my/PlayerActivity.kt
+++ exoplayer-codelab-my/src/main/java/com/ostapr/exoplayer_codelab_my/PlayerActivity.kt
@@ -16,13 +16,34 @@
 package com.ostapr.exoplayer_codelab_my
 
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.analytics.AnalyticsListener
+import com.example.exoplayer.R
 import com.example.exoplayer.databinding.ActivityPlayerBinding
+import com.ostapr.exoplayer_codelab_my.PlayerActivity.Companion.TAG
 
 /**
  * A fullscreen activity to play audio or video streams.
  */
 class PlayerActivity : AppCompatActivity() {
+
+    private var player: ExoPlayer? = null
+    private var playWhenReady = true
+    private var mediaItemIndex = 0
+    private var playbackPosition = 0L
+    private val playerListener = playbackStateListener()
+    private val analyticsListener = analyticsListener()
 
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityPlayerBinding.inflate(layoutInflater)
@@ -31,5 +52,142 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+    }
+
+    @OptIn(UnstableApi::class)
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    @OptIn(UnstableApi::class)
+    override fun onResume() {
+        super.onResume()
+
+        hideSystemUi()
+
+        if (Util.SDK_INT <= 23 || player == null) {
+            initializePlayer()
+        }
+    }
+
+    @OptIn(UnstableApi::class)
+    override fun onPause() {
+        super.onPause()
+
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    @OptIn(UnstableApi::class)
+    override fun onStop() {
+        super.onStop()
+
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(this)
+            .build()
+            .also { exoPlayer ->
+                viewBinding.videoView.player = exoPlayer
+
+                exoPlayer.trackSelectionParameters = exoPlayer.trackSelectionParameters
+                    .buildUpon()
+                    .setMaxVideoSizeSd()
+                    .build()
+            }
+
+        player?.let(::restorePlayerState)
+    }
+
+    private fun restorePlayerState(player: ExoPlayer) {
+        val audioItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
+        val videoItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+        val dashVideoItem = MediaItem.Builder()
+            .setUri(getString(R.string.media_url_dash))
+            .setMimeType(MimeTypes.APPLICATION_MPD)
+            .build()
+
+        player.setMediaItems(
+            listOf(audioItem, videoItem, dashVideoItem),
+            mediaItemIndex,
+            playbackPosition
+        )
+        player.playWhenReady = playWhenReady
+        player.addListener(playerListener)
+        player.addAnalyticsListener(analyticsListener)
+        player.prepare()
+    }
+
+    private fun releasePlayer() {
+        player?.let { exoPlayer ->
+            playbackPosition = exoPlayer.currentPosition
+            mediaItemIndex = exoPlayer.currentMediaItemIndex
+            playWhenReady = exoPlayer.playWhenReady
+
+            exoPlayer.removeListener(playerListener)
+            exoPlayer.release()
+        }
+        player = null
+    }
+
+    companion object {
+        const val TAG = "PlayerActivity"
+    }
+}
+
+@OptIn(UnstableApi::class)
+private fun analyticsListener() = object : AnalyticsListener {
+    override fun onRenderedFirstFrame(
+        eventTime: AnalyticsListener.EventTime,
+        output: Any,
+        renderTimeMs: Long
+    ) {
+        Log.d(TAG, "ExoPlayer - First frame in $renderTimeMs")
+    }
+
+    override fun onDroppedVideoFrames(
+        eventTime: AnalyticsListener.EventTime,
+        droppedFrames: Int,
+        elapsedMs: Long
+    ) {
+        Log.w(TAG, "ExoPlayer - Dropped video frames for $elapsedMs")
+    }
+
+    override fun onAudioUnderrun(
+        eventTime: AnalyticsListener.EventTime,
+        bufferSize: Int,
+        bufferSizeMs: Long,
+        elapsedSinceLastFeedMs: Long
+    ) {
+        Log.w(TAG, "ExoPlayer - Audio underrun is nore noticeable then video frames drop for $elapsedSinceLastFeedMs")
+    }
+}
+
+private fun playbackStateListener() = object : Player.Listener {
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        val stateString: String = when (playbackState) {
+            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
+            ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
+            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
+            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
+            else -> "UNKNOWN_STATE             -"
+        }
+        Log.d(TAG, "changed state to $stateString")
     }
 }
