537d5fd1cd6e3299aefd0f0094d8985fece44796
Ostap <revakooa@gmail.com>
2023-08-24 18:46:24 +0300

    Add ExoPlayer initialization.
     - ExoPlayer is created, bound to view and filled with 1 MediaItem to play the sound.

--- exoplayer-codelab-my/src/main/java/com/ostapr/exoplayer_codelab_my/PlayerActivity.kt
+++ exoplayer-codelab-my/src/main/java/com/ostapr/exoplayer_codelab_my/PlayerActivity.kt
@@ -16,13 +16,24 @@
 package com.ostapr.exoplayer_codelab_my
 
 import android.os.Bundle
+import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
+import com.example.exoplayer.R
 import com.example.exoplayer.databinding.ActivityPlayerBinding
 
 /**
  * A fullscreen activity to play audio or video streams.
  */
 class PlayerActivity : AppCompatActivity() {
+
+    private var player: ExoPlayer? = null
 
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityPlayerBinding.inflate(layoutInflater)
@@ -31,5 +42,68 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+    }
+
+    @OptIn(UnstableApi::class)
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    @OptIn(UnstableApi::class)
+    override fun onResume() {
+        super.onResume()
+
+        hideSystemUi()
+
+        if (Util.SDK_INT <= 23 || player == null) {
+            initializePlayer()
+        }
+    }
+
+    @OptIn(UnstableApi::class)
+    override fun onPause() {
+        super.onPause()
+
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    @OptIn(UnstableApi::class)
+    override fun onStop() {
+        super.onStop()
+
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(this)
+            .build()
+            .also { exoPlayer -> viewBinding.videoView.player = exoPlayer }
+
+        populatePlayerWithSampleMusic()
+    }
+
+    private fun populatePlayerWithSampleMusic() {
+        val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
+        player?.setMediaItem(mediaItem)
+    }
+
+    private fun releasePlayer() {
+        player?.release()
+        player = null
     }
 }
